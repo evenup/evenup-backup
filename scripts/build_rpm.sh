dc7bea79031d0b6300b9c5dceb87ca9c36dcb9f6
Justin Lambert <jlambert@eml.cc>
2015-01-05 18:42:54 -0700

    update rpm script, documentation

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-# Clone the repo
-git clone https://github.com/meskyanichi/backup.git
+if [ -z $1 ] ; then
+  echo "Please run with version to build:  ${0} <version>"
+  exit 1
+fi
 
-cd backup
+# Clone the repo
+if [ -d backup ] ; then
+  cd backup
+  git fetch
+else
+  git clone https://github.com/meskyanichi/backup.git
+  cd backup
+fi
+
+git checkout $1
 
 # Set up bundler
 sed -i "3i $: << File.join(File.dirname(__FILE__), '..',  \"lib\")" lib/backup.rb
-sed -i "5i require 'rubygems'" lib/backup.rb
-sed -i "6i require 'bundler/setup'" lib/backup.rb
-# Add hostname to hipchat output
-sed -i '/message = "\[Backup::%s\] #{@model.label} (#{@model.trigger})" % name/c message = "[Backup::%s] #{@model.label} (#{@model.trigger}@#{Socket.gethostname})" % name' lib/backup/notifier/hipchat.rb
+#sed -i "6i require 'bundler/setup'" lib/backup.rb
 
 # Bundle
-bundle install --path=vendor/bundle
-bundle install --binstubs
 bundle install --standalone
+#bundle install --binstubs=/usr/local/bin --standalone
 cd ..
 
+mkdir tmp
+
+echo "cat > /usr/local/bin/backup <<EOF" > tmp/install_bin.sh
+echo "#!/usr/bin/env ruby" >> tmp/install_bin.sh
+echo "# encoding: utf-8" >> tmp/install_bin.sh
+echo "" >> tmp/install_bin.sh
+echo "require File.expand_path(\"/opt/backup/lib/backup\", __FILE__)" >> tmp/install_bin.sh
+echo "Backup::CLI.start" >> tmp/install_bin.sh
+echo "EOF" >> tmp/install_bin.sh
+echo "" >> tmp/install_bin.sh
+echo "chmod 0555 /usr/local/bin/backup" >> tmp/install_bin.sh
+
+echo "rm -f /usr/local/bin/backup" > tmp/remove_bin.sh
 # Build the package
 fpm \
   -s dir \
   -t rpm \
-  -v 3.5.1 \
+  -v ${1} \
   --iteration '1' \
   -n rubygem-backup \
   -a noarch \
@@ -31,7 +51,10 @@ fpm \
   --prefix /opt/backup \
   --rpm-user root \
   --rpm-group root \
+  --after-install tmp/install_bin.sh \
+  --after-remove tmp/remove_bin.sh \
   -x opt/backup/.git* \
-  -d rubygem-bundler \
   -C backup .
 
+rm -rf tmp
+#  -d rubygem-bundler \
